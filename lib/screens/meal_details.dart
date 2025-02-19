73b4f0a73f5af7d85a543ddc48578f00563cf3cb
hotabya <hotabya.aman@gmail.com>
2025-02-19 12:39:16 +0530

    Riverpod

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -1,25 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/providers/favorites_provider.dart';
 
-class MealDetailsScreen extends StatelessWidget {
-  const MealDetailsScreen({super.key, required this.meal, required this.onTogglefavorite});
+class MealDetailsScreen extends ConsumerWidget {
+  const MealDetailsScreen({super.key, required this.meal});
 
   final Meal meal;
-  final void Function(Meal meal) onTogglefavorite;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+
+    final isFavorite = favoriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
         actions: [
           IconButton(
             onPressed: () {
-              onTogglefavorite(meal);
-            }, 
-            icon: Icon(Icons.star),
-          )
-        ],  
+              final wasAdded = ref
+                  .read(favoriteMealsProvider.notifier)
+                  .toggleMealFavoriteStatus(meal);
+              ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text(wasAdded ? 'Meal added as a favorite.' : 'Meal removed.')));
+            },
+            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -46,8 +57,8 @@ class MealDetailsScreen extends StatelessWidget {
                   color: Theme.of(context).colorScheme.onBackground,
                 ),
               ),
-              const SizedBox(height: 24),
-              Text(
+            const SizedBox(height: 24),
+            Text(
               'Steps',
               style: Theme.of(context).textTheme.titleLarge!.copyWith(
                 color: Theme.of(context).colorScheme.primary,
@@ -57,7 +68,10 @@ class MealDetailsScreen extends StatelessWidget {
             const SizedBox(height: 14),
             for (final step in meal.steps)
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 8,
+                ),
                 child: Text(
                   step,
                   textAlign: TextAlign.center,
@@ -65,7 +79,7 @@ class MealDetailsScreen extends StatelessWidget {
                     color: Theme.of(context).colorScheme.onBackground,
                   ),
                 ),
-              )
+              ),
           ],
         ),
       ),
--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -4,17 +4,15 @@ import 'package:meals/screens/meal_details.dart';
 import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, this.title, required this.meals, required this.onTogglefavorite});
+  const MealsScreen({super.key, this.title, required this.meals, });
 
   final String? title;
   final List<Meal> meals;
-  final void Function(Meal meal) onTogglefavorite;
 
   void selectmeal(BuildContext context, Meal meal) {
     Navigator.of(context).push(
       MaterialPageRoute(builder: (ctx) => MealDetailsScreen(
         meal: meal,
-        onTogglefavorite: onTogglefavorite,
         ),
       ),
     );
